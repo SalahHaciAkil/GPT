cd6e6658f033b3527d22ddb6cbfde14da53304c3
salahHaciAkil <your_email@abc.com>
2023-05-19 16:42:06 +0300

    analysing user comments

--- f.py
+++
@@ -1,2 +0,0 @@
-# sssssssssssssssssssssssssssssss
-# The end
--- script.py
+++ script.py
@@ -1,14 +1,2 @@
-# Import the OpenAI library
-import openai
 
-
-# Set the API key
-
-key ="key"
-
-openai.api_key = key
-
-# ask the user what he want to search on the internet
-search = input("What do you want to search on the internet? ")
-# go and get the first result from google 
-firstResult = googleSearch(search)
+print('Hello GPT !!')
