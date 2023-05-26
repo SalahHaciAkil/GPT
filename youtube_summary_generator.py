7368d97cabf3926453678007b1b49310c9839e76
salahHaciAkil <your_email@abc.com>
2023-05-26 23:00:33 +0300

    youtube summary generator

---
+++ youtube_summary_generator.py
@@ -0,0 +1,98 @@
+import openai
+import os
+from pytube import YouTube
+import re
+with open('openai_key.txt', 'r') as f:
+    api_key = f.read().strip('\n')
+    assert api_key.startswith('sk-'), "Please enter a valid OpenAI API key"
+openai.api_key = api_key
+
+def youtube_audio_downloader(link):
+    if "youtube.com" not in link:
+        print("Please enter a valid YouTube URL")
+        return False
+    yt = YouTube(link)
+    print(f'Title of the video: {yt.title}')
+    print(f'Length of the video: {yt.length} seconds')
+    
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    download_result = audio_stream.download(
+        output_path='audios',
+    )
+    
+    if(os.path.exists(download_result)):
+        print("File downloaded successfully" )
+    else:
+        print("Some error occurred")
+        return False
+
+    basename = os.path.basename(download_result)
+
+    name, extension = os.path.splitext(basename)
+    audio_file  = re.sub('\s+', '-',  name)
+    complete_audio_path = f'{audio_file}.mp3'
+    os.rename(download_result, complete_audio_path)
+    return complete_audio_path
+
+def transcribe(audio_file):
+    if not os.path.exists(audio_file):
+        print(f'The following file doensn not exist: {audio_file}')
+        return False
+    with open(audio_file, 'rb') as f:
+        print("Starting transcribing ...", end='')
+        transcript = openai.Audio.transcribe('whisper-1', f)
+
+        print('Done!')
+        
+        
+        name, extension = os.path.splitext(audio_file)
+        transcript_filename = f'transcript-{name}.text'
+        with open(transcript_filename, 'w') as f:
+            f.write(transcript['text'])
+    
+    return transcript_filename
+
+
+def summarize(transcript_filename):
+    if not os.path.exists(transcript_filename):
+        print("The transcript file doesn't exist!")
+        return False
+    
+    with open(transcript_filename) as f:
+        transcript = f.read()
+        
+        
+    system_prompt = "Act as Expert one who can summarize any topic"
+    prompt = f'''Create a summary of the following text.
+    Text{transcript}
+    
+    Add a title to the summary.
+    Your summary should be informative and factual, covering the most important aspects if the topic.
+    Use BULLET POINTS if possible'''
+    
+    
+    
+    print("Start summarizing....", end=" ")
+    
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {'role':'system', 'content': system_prompt},
+            {'role':'user', 'content':prompt}
+        ],
+        max_tokens=2024,
+        temperature=1
+    
+    )
+    
+    print('Done!')
+    r = response['choices'][0]['message']['content']
+    return r
+
+
+link = 'YouTube link'
+downloaded_audio_file = youtube_audio_downloader(link)
+transcribed_file = transcribe(downloaded_audio_file)
+summary = summarize(transcribed_file)
+print('\n')
+print(summary)
